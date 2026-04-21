4e1d7c5ef8852589fb533fed8540b03ecd339042
Alex Haskins <lxhaskins@gmail.com>
2026-04-21 14:02:39 -0700

    Added sensor testing logic to main (will be changed later)

--- main.cpp
+++ main.cpp
@@ -1,2 +1,40 @@
 #include "mbed.h"
+#include <chrono>
 
+DigitalOut trig(PA_9);
+DigitalIn echo(PA_8);
+
+Timer timer;
+
+int main() {
+    printf("Ultrasonic Sensor Test\r\n");
+
+    while (true) {
+        // Ensure trigger is low
+        trig = 0;
+        wait_us(2);
+
+        // Send 10us pulse
+        trig = 1;
+        wait_us(10);
+        trig = 0;
+
+        // Wait for echo to go HIGH
+        while (echo == 0);
+
+        timer.reset();
+        timer.start();
+
+        // Wait for echo to go LOW
+        while (echo == 1);
+
+        timer.stop();
+
+        float time = timer.read(); // seconds
+        float distance = (time * 34300.0f) / 2.0f;
+
+        printf("Distance: %.2f cm\r\n", distance);
+
+        ThisThread::sleep_for(500);
+    }
+}
